7138a294f63fbb9ffec65076bfe7ba17a3d877d4
mushfik <smrahman0009@gmail.com>
2021-05-21 17:06:11 +0600

    redux broilerplalate setup

---
+++ src/js/action/index.js
@@ -0,0 +1,5 @@
+import {ADD_ARTICLE} from "../constant/action-types"
+
+export function addArticle(payload){
+    return {type:ADD_ARTICLE,payload}
+}
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import store from "../js/store/index";
+import { addArticle } from "../js/action/index";
+
+window.store = store;
+window.addArticle = addArticle;
