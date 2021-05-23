566d8d95ca299517e336d2481beed01479432c3b
mushfik <smrahman0009@gmail.com>
2021-05-23 21:48:22 +0600

    redux setup done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Counter from "./Counter";
-import { Provider } from 'react-redux';
+import {useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state)=>state.counter.count)
   return (
-    <Router>
+      <div>
+        <h1>Counter {count}</h1>
       <Counter/>
-     </Router>
+      </div>
   );
 }
 
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import { decrement, increment } from "./redux/ducks/counter";
 
 const Counter=()=>{
     
-    const count = useSelector((state)=>state.counter.count)
     const dispatch = useDispatch()
     const handleIncrement=()=>{
        dispatch(increment()) 
@@ -14,8 +13,7 @@ const Counter=()=>{
     }
     return (
         <div style={{ backgroundColor: "grey", margin: "10px" }}>
-          <h2>{`Count: ${count}`}</h2>
-          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
             <button onClick={handleIncrement}> Increment </button>
             <button onClick={hadndleDecrement}> Decrement </button>
           </div>
