2bb760228fa6506eb7dd57999f7c7b900b096f9c
mushfik <smrahman0009@gmail.com>
2021-05-24 21:56:04 +0600

    ridux saga setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Counter from "./Counter";
-import {useSelector } from 'react-redux';
+import {useDispatch, useSelector } from 'react-redux';
+import { getUser } from './redux/ducks/user';
 
 function App() {
   const count = useSelector((state)=>state.counter.count)
   const voterList = ["Talha Chowdhury","Anik Sarkar","Imam Azam","Sudipta Ranjan"]
-  
+
+
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getUser())
+  },[dispatch])
+
+  const user = useSelector((state)=>(state.user.user))
+  console.log(user)
   return (
       <div>
         <h1>Total Vote{count}</h1>
---
+++ src/redux/sagas/requests/user.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+export function requestGetUser(){
+    return axios.request(
+        {
+            method: "get",
+            url:"https://jsonplaceholder.typicode.com/users/2"
+        }
+    )
+}
